7073e602342f5633e4393595508db7d88a5f000e
Keqi Chen <ckq009@gmail.com>
2023-03-01 16:20:04 +0000

    Update datatype

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -5,14 +5,14 @@ public class BookModel
     public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
-    public string CheckOut { get; set; }
-    public string CheckIn { get; set; }
-    // public Nullable<DateTime> CheckOut { get; set; }
-    // public Nullable<DateTime> CheckIn { get; set; }
+    // public string CheckOut { get; set; }
+    // public string CheckIn { get; set; }
+    public Nullable<DateTime> CheckOut { get; set; }
+    public Nullable<DateTime> CheckIn { get; set; }
     public Nullable<int> MemberId { get; set; }
 
 //still not nullable;
-    public BookModel (int id, string title, string author, string checkOut, string checkIn, Nullable<int> memberId){
+    public BookModel (int id, string title, string author, Nullable<DateTime> checkOut, Nullable<DateTime> checkIn, Nullable<int> memberId){
         Id = id;
         Title = title;
         Author = author;
@@ -26,7 +26,7 @@ public class BookModel
         Author=author;
     }
 
-    public BookModel (int id, string checkOut, Nullable<int> memberId){
+    public BookModel (int id, Nullable<DateTime> checkOut, Nullable<int> memberId){
         Id = id;
         CheckOut = checkOut;
         MemberId = memberId;
--- Models/CheckoutModel.cs
+++ Models/CheckoutModel.cs
@@ -3,7 +3,7 @@ namespace Bookish.Models;
 public class CheckOutModel
 {
     public int BookId { get; set; }
-    public string CheckOut { get; set; }
+    public Nullable<DateTime> CheckOut { get; set; }
     public Nullable<int> MemberId { get; set; }
 }
 
